2fac10b4d0061d43feb7b8a8ba6f2ae4fb1a98f3
Ossey <xiaoyuan1314@me.com>
2017-10-07 09:35:35 +0800

    Initial commit

---
+++ WeChatPlugin/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChatPlugin
+//
+//  Created by Swae on 2017/10/7.
+//  Copyright © 2017年 Ossey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WeChatPlugin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WeChatPlugin
+//
+//  Created by Swae on 2017/10/7.
+//  Copyright © 2017年 Ossey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
